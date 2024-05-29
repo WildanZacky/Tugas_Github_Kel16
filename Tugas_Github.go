a29240e193db0ae77e9d51078f30148067bda130
Somus88 <96776245+Somus88@users.noreply.github.com>
2024-05-29 20:55:39 +0700

    Create Tugas_Github.go

---
+++ Tugas_Github.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Data struct untuk menyimpan informasi kendaraan
+type Data struct {
+	Nama string
+	Plat string
+	Tipe string
+	Rute []string
+}
+
+// Pelanggaran struct untuk menyimpan informasi pelanggaran
+type Pelanggaran struct {
+	Name   string
+	Tilang int
+}
+
+// Function kenaRazia untuk memeriksa pelanggaran aturan ganjil-genap
+func kenaRazia(tanggal int, data []Data) []Pelanggaran {
+	// Lokasi Ganjil Genap
+	ruteGanjilGenap := []string{"Gajah Mada", "Hayam Wuruk", "Sisingamangaraja", "Panglima Polim", "Fatmawati", "Tomang Raya"}
+
+	var hasil []Pelanggaran
+
+	for _, kendaraan := range data {
+		// Memeriksa jenis kendaraan, hanya MOBIL yang diperiksa
+		if kendaraan.Tipe != "Mobil" && kendaraan.Tipe != "mobil" {
+			continue
+		}
+
+		// Mendapatkan digit terakhir dari nomor plat kendaraan
+		var angkaTerakhirChar byte
+		for i := len(kendaraan.Plat) - 1; i >= 0; i-- {
+			if kendaraan.Plat[i] >= '0' && kendaraan.Plat[i] <= '9' {
+				angkaTerakhirChar = kendaraan.Plat[i]
+				break
+			}
+		}
+
+		// Memeriksa apakah nomor plat sesuai dengan aturan ganjil-genap pada tanggal tersebut
+		angkaTerakhir := int(angkaTerakhirChar - '0')
+		tanggalGanjil := tanggal%2 != 0
+		platGanjil := angkaTerakhir%2 != 0
+		pelanggaranN := 0
